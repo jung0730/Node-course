9bbe17418fd495c3c3ed67b1375bb5814088d623
rebecca <rebecca.yu@senao.com.tw>
2023-04-19 23:17:20 +0800

    登入註冊

--- Lesson6/routes/users.js
+++ Lesson6/routes/users.js
@@ -1,8 +1,52 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, createUsers } = require("../controllers/users");
+const User = require('../models/users');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-router.get("/", getUsers);
-router.post("/", createUsers);
+const errorHandler = require('../service/errorHandler');
+const successHandler = require('../service/successHandler');
+const asyncErrorHandler = require('../service/asyncErrorHandler');
+const { isAuth, generateJWT } = require('../service/auth');
 
-module.exports = router;
+router.post(
+  '/sign_up',
+  asyncErrorHandler(async (req, res, next) => {
+    const { name, email, confirmPassword } = req.body;
+    let { password } = req.body;
+    if (!name || !email || !password) return next(errorHandler(400,"欄位未填寫正確"));
+    // 密碼是否有大於 8 碼
+    if (!validator.isLength(password, { min: 8 })) {
+      return next(errorHandler(400,"密碼不得低於 8 字元"));
+    }
+    if (password !== confirmPassword) return next(errorHandler(400,"密碼不一致"));
+    // 是否為 Email 格式
+    if (!validator.isEmail(email)) return next(errorHandler(400,"信箱格式錯誤"));
+    // 密碼加密
+    password = await bcrypt.hash(password, 12);
+    const data = await User.create({
+      name,
+      email,
+      password,
+    });
+    generateJWT(data, res);
+  })
+);
+
+router.post(
+  '/sign_in',
+  asyncErrorHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(errorHandler(400,"帳號密碼不可為空"));
+    }
+    // 如果omit '.select('+password')'
+    // The returned object will only contain properties that are explicitly included in the schema, and any excluded fields (such as the password field, in this case) will be omitted.
+    const data = await User.findOne({ email }).select('+password');
+    const auth = await bcrypt.compare(password, data.password);
+    if (!auth) {
+      return next(errorHandler(400,"密碼錯誤"));
+    };
+    generateJWT(data, res);
+  })
+);
---
+++ Lesson6/service/auth.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+const successHandler = require('../service/successHandler');
+const errorHandler = require('../service/errorHandler');
+const asyncErrorHandler = require('../service/asyncErrorHandler');
+
+const isAuth = asyncErrorHandler(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split('')[1];
+  }
+  if (!token) {
+    return next(errorHandler(401,"尚未登入"));
+  }
+  const decoded = await new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(payload);
+      }
+    });
+  });
+  const currentUser = await User.findById(decoded.id);
+  req.user = currentUser;
+  next();
+});
+
+const generateJWT = (user, res) => {
+  const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_DAY,
+  });
+  successHandler(res, {
+    token,
+    name: user.name,
+  });
+}
+
+module.exports = {
+  isAuth,
+  generateJWT,
+}
